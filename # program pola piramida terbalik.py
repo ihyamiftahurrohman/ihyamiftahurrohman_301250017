7bab6ec86b31410950ab525eba49281046f01b13
ihyamiftahurrohman <ihyamr59@gmail.com>
2025-10-22 17:29:58 +0700

    Delete # program pola piramida terbalik.py

--- # program pola piramida terbalik.py
+++
@@ -1,6 +0,0 @@
-# program pola piramida terbalik
-
-baris = int(input("masukkan jumlah baris"))
-
-for i in range (baris, 0, -1): # muali dari 'baris" hingga 1
-    print('*' * i) # cetak '*' sebanyak i
